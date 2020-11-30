f7d3b5fd46fffb959eb2dfbdcf61a5254972baab
LiWenBo <liwenbo4922@163.com>
2020-11-30 21:30:50 +0800

    Initial commit

---
+++ src/main/java/com/li/mysql_lesson/MysqlLessonApplication.java
@@ -0,0 +1,13 @@
+package com.li.mysql_lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlLessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/li/mysql_lesson/MysqlLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.mysql_lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
